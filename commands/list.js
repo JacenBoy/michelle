64fffa5607036626833a138d4875ebedb7b7e972
JacenBoy <jacenboy1@gmail.com>
2019-01-23 16:01:15 -0500

    list command even more completed

--- commands/list.js
+++ commands/list.js
@@ -12,15 +12,25 @@ exports.run = async (client, message, args, level) => {
     var curprofile = client.profiles.get(usermention.id);
     if (!curprofile.lists) return message.channel.send(`No profile found for ${usermention.username}`);
     var fieldarray = [];
+    var i = 0;
     if (curprofile.lists.kitsu) {
-      fieldarray[0] = {"name": "Kitsu", "value": curprofile.lists.kitsu};
+      fieldarray[i] = {"name": "Kitsu", "value": `[${curprofile.lists.kitsu}](https://kitsu.io/users/${curprofile.lists.kitsu})`};
+      i++;
     }
     if (curprofile.lists.mal) {
-      fieldarray[0] = {"name": "MAL", "value": curprofile.lists.mal};
+      fieldarray[i] = {"name": "MyAnimeList", "value": `[${curprofile.lists.mal}](https://myanimelist.net/profile/${curprofile.lists.mal})`};
+      i++;
     }
     if (curprofile.lists.anilist) {
-      fieldarray[0] = {"name": "AniList", "value": curprofile.lists.anilist};
+      fieldarray[i] = {"name": "AniList", "value": `[${curprofile.lists.anilist}](https://anilist.co/users/${curprofile.lists.anilist})`};
+      i++;
     }
+    var embed = { "embed": {
+      "title": `${usermention.username}'s Anime Lists`,
+      "thumbnail": { "url": usermention.avatarURL || usermention.defaultAvatarURL },
+      "fields": fieldarray
+    } };
+    message.channel.send(embed);
   } else {
     // Add mode - Add a list to the user's profile
     client.profiles.ensure(message.author.id, {
@@ -28,17 +38,18 @@ exports.run = async (client, message, args, level) => {
     });
     switch (args[0].toLowerCase()) {
       case 'kitsu':
-        client.profiles.set(message.author.id, args[1], "lists.kitsu");
+        client.profiles.set(message.author.id, args[1] || false, "lists.kitsu");
         break;
       case 'mal':
-        client.profiles.set(message.author.id, args[1], "lists.mal");
+        client.profiles.set(message.author.id, args[1] || false, "lists.mal");
         break;
       case 'anilist':
-        client.profiles.set(message.author.id, args[1], "lists.anilist");
+        client.profiles.set(message.author.id, args[1] || false, "lists.anilist");
         break;
       default:
         return message.channel.send(`Improper format. Use \`${client.getSettings(message.guild.id).prefix}help list\` for assistance.`);
     }
+    message.channel.send('Anime list has been updated.');
   }
 };
   
