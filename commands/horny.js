507d330a25667eeea781cb334bc3199aa14895f6
JacenBoy <jacenboy1@gmail.com>
2019-05-07 13:49:53 -0400

    Super top secret update

--- commands/horny.js
+++ commands/horny.js
@@ -4,14 +4,21 @@ require("moment-duration-format");
 exports.run = async (client, message, args, level) => {
   if (client.config.specialServers.indexOf(message.guild.id) == -1) return;
   if (!message.mentions.users.first()) return;
-  const user = message.mentions.users.first();
-  if (!client.horny.has(user.id)) {
-    client.horny.set(user.id, moment());
-    message.channel.send(`This is the first officially recorded time ${user.username} has been horny`);
-  } else {
-    message.channel.send(`It has been ${moment.duration(moment().diff(client.horny.get(user.id))).format(" D [days], H [hrs], m [mins], s [secs]")} since ${user.username} was last horny.`);
-    client.horny.set(user.id, moment());
-  }
+  var fields = [];
+  var i = 0;
+  message.mentions.users.array().forEach(u => {
+    if (!client.horny.has(u.id)) {
+      client.horny.set(u.id, {"lastTime": moment(), "totalCount": 1});
+      fields[i] = {"name": `${u.username} horny count: 1`, "value": `This is the first recorded time ${u.username} has been horny`};
+    } else {
+      const udata = client.horny.get(u.id);
+      udata.totalCount++;
+      fields[i] = {"name": `${u.username} horny count: ${udata.totalCount}`, "value": `It has been ${moment.duration(moment().diff(udata.lastTime)).format("D [days] H [hours] m [minutes] s [seconds]")} since ${u.username} was last horny`};
+      client.horny.set(u.id, {"lastTime": moment(), "totalCount": udata.totalCount});
+    }
+    i++;
+  });
+  message.channel.send({"embed": {"fields": fields}});
 };
 
 exports.conf = {
