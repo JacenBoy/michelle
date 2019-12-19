754b699ea9bab3d13bf09b10814d1b2dce856368
JacenBoy <jacenboy1@gmail.com>
2019-12-18 21:58:43 -0500

    Final tweaks to clip command

--- commands/clip.js
+++ commands/clip.js
@@ -2,12 +2,14 @@
 
 exports.run = async (client, message, args, level) => {
   var randclip = client.clips.random();
+  if (!args[0]) args[0] = "en";
   
   const embed = {"embed":{
     "title": `${randclip.title} ${randclip.isSpoiler ? "(Spoiler)" : ""}`,
-    "description": `${randclip.source} S${client.pad(randclip.season, 2)}E${client.pad(randclip.episode, 2)} ${randclip.isSpoiler ? "(Spoiler)" : ""}`,
+    "description": `_${randclip.source}_ S${client.pad(randclip.season, 2)} E${client.pad(randclip.episode, 2)} ${randclip.isSpoiler ? "(Spoiler)" : ""}`,
     "url": `https://streamable.com/${args[0].toLowerCase() == "jp" ? randclip.jpid || randclip.id : randclip.id}`,
-    "image": {"url": `https://michelle.jacenboy.com/assets/thumbnails/${randclip.id}.png`}
+    "image": {"url": `https://michelle.jacenboy.com/assets/thumbnails/${randclip.id}.png`},
+    "color": client.colorInt("#3e3c73")
   }};
   message.channel.send(embed);
 };
