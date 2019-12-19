b51c72f764fb3389b4179ee9eabe7aebbcdae556
JacenBoy <jacenboy1@gmail.com>
2019-12-18 21:42:23 -0500

    Added clips + more obvious spoiler warning

--- commands/clip.js
+++ commands/clip.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args, level) => {
   var randclip = client.clips.random();
   
   const embed = {"embed":{
-    "title": randclip.title,
+    "title": `${randclip.title} ${randclip.isSpoiler ? "(Spoiler)" : ""}`,
     "description": `${randclip.source} S${client.pad(randclip.season, 2)}E${client.pad(randclip.episode, 2)} ${randclip.isSpoiler ? "(Spoiler)" : ""}`,
     "url": `https://streamable.com/${args[0].toLowerCase() == "jp" ? randclip.jpid || randclip.id : randclip.id}`,
     "image": {"url": `https://michelle.jacenboy.com/assets/thumbnails/${randclip.id}.png`}
