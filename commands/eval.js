12502d2164cadbe910253e2ad675ffaea869d33d
JacenBoy <jacenboy1@gmail.com>
2021-07-29 17:05:45 -0400

    Reverting eval command because I changed my mind

--- commands/eval.js
+++ commands/eval.js
@@ -5,46 +5,45 @@
 
 // However it's, like, super ultra useful for troubleshooting and doing stuff
 // you don't want to put in a command.
-const Command = require("../base/Command.js");
-
-class Eval extends Command {
-  constructor (client) {
-    super(client, {
-      name: "eval",
-      category: "System",
-      description: "Evaluates arbitrary javascript.",
-      usage: "eval [...code]",
-      permLevel: "Bot Owner"
-    });
-  }
-
-  async run (message, args, level) {
-    const fetch = require("node-fetch");
-    const code = args.join(" ");
-    try {
-      const evaled = eval(code);
-      const clean = await this.client.clean(this.client, evaled);
-      if (clean.length > 1090) { 
-        try {
-          const {key} = await fetch("https://hasteb.in/documents", {
-            method: "POST",
-            body: clean,
-            headers: {"Content-Type": "application/json"}
-          }).then(res => res.json());
-          if (!key) throw "Error posting the response";
-          message.channel.send(`\`\`\`\nResponse too long. Uploaded output to https://hasteb.in/${key}.js.\n\`\`\``);
-        } catch (ex) {
-          this.client.logger.debug(clean);
-          message.channel.send("```\nResponse too long. Check the console for full output.\n```");
-        }
-        
-      } else {
-        message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
+exports.run = async (client, message, args, level) => {
+  const fetch = require("node-fetch");
+  const code = args.join(" ");
+  try {
+    const evaled = eval(code);
+    const clean = await client.clean(client, evaled);
+    if (clean.length > 1090) { 
+      try {
+        const {key} = await fetch("https://hasteb.in/documents", {
+          method: "POST",
+          body: clean,
+          headers: {"Content-Type": "application/json"}
+        }).then(res => res.json());
+        if (!key) throw "Error posting the response";
+        message.channel.send(`\`\`\`\nResponse too long. Uploaded output to https://hasteb.in/${key}.js.\n\`\`\``);
+      } catch (ex) {
+        client.logger.debug(clean);
+        message.channel.send("```\nResponse too long. Check the console for full output.\n```");
       }
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${await this.client.clean(this.client, err)}\n\`\`\``);
+      
+    } else {
+      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
     }
+  } catch (err) {
+    message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
   }
-}
+};
 
-module.exports = Eval;
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  special: false,
+  aliases: [],
+  permLevel: "Bot Owner"
+};
+
+exports.help = {
+  name: "eval",
+  category: "System",
+  description: "Evaluates arbitrary javascript.",
+  usage: "eval [...code]"
+};
