ba5daad0825ec0e7c226854ddc1e20502294e235
JacenBoy <jacenboy1@gmail.com>
2021-08-11 16:28:16 -0400

    Implementing permissions check

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,8 +7,20 @@ module.exports = async (client, interaction) => {
   // Ignore if the command doesn't exist
   if (!cmd) return;
 
+  // Get the server settings
+  const settings = client.config.defaultSettings;
+
+  // If the member on a guild is invisible or not cached, fetch them.
+  if (interaction.guild && !interaction.member) await interaction.guild.members.fetch(interaction.user);
+  // Get the user or member's permission level from the elevation
+  const level = client.permlevel(interaction);
+  if (level < client.levelCache[cmd.conf.permLevel]) {
+    return interaction.reply({content: "You do not have sufficient permissions to run this command", ephemeral: true});
+  }
+
   // Try to run the command
   try {
+    client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${interaction.user.username} (${interaction.user.id}) ran command ${cmd.help.name}`);
     await cmd.run(interaction);
   } catch (ex) {
     client.logger.error(ex);
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -68,6 +68,6 @@ module.exports = async (client, message) => {
   
   // If the command exists, **AND** the user has permission, run it.
   client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`);
-  message.reply(`As of April 2022, Discord will be removing bot access to reading messages, meaning \`m-\` prefixed commands will no longer work. Please begin using the equivalent command \`/${cmd.help.name}\``);
+  message.reply(`As of April 2022, Discord will be removing bot access to reading messages, meaning \`${settings.prefix}\` prefixed commands will no longer work. Please begin using the equivalent command \`/${cmd.help.name}\``);
   cmd.run(client, message, args, level);
 };
