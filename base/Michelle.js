53896fda9b59168454ac9eba792542e17e91b3d3
JacenBoy <jacenboy1@gmail.com>
2021-08-11 19:01:30 -0400

    Adding error checking to command deployment

--- base/Michelle.js
+++ base/Michelle.js
@@ -76,17 +76,21 @@ class Michelle extends Client {
   async deployCommands () {
     this.guilds.cache.map(guild => {
       this.slashCommands.map(async (cmd) => {
-        if (!cmd.conf.enabled) return; // Do not deploy disabled commands
-        if (cmd.conf.global) {
-          // Global command deployment
-        } else {
-          if (cmd.conf.special && !cmd.conf.special.includes(guild.id)) return; // Do not deploy server-restricted commands
-          const data = {
-            "name": cmd.help.name,
-            "description": cmd.help.description,
-            "options": cmd.conf.options
-          };
-          await this.guilds.cache.get(guild.id)?.commands.create(data);
+        try {
+          if (!cmd.conf.enabled) return; // Do not deploy disabled commands
+          if (cmd.conf.global) {
+            // Global command deployment
+          } else {
+            if (cmd.conf.special && !cmd.conf.special.includes(guild.id)) return; // Do not deploy server-restricted commands
+            const data = {
+              "name": cmd.help.name,
+              "description": cmd.help.description,
+              "options": cmd.conf.options
+            };
+            await this.guilds.cache.get(guild.id)?.commands.create(data);
+          }
+        } catch (ex) {
+          this.logger.error(`Error deploying command ${cmd.help.name} to guild ${guild.name}: ${ex}`);
         }
       });
     });
