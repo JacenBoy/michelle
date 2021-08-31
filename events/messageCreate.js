860d7d84db3b6d4a67f5101ca779691501bc4bd2
JacenBoy <jacenboy1@gmail.com>
2021-08-31 08:59:50 -0400

    Update deprecated command logging

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -67,7 +67,7 @@ module.exports = async (client, message) => {
   }
   
   // If the command exists, **AND** the user has permission, run it.
-  client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`);
+  client.logger.warn(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran deprecated command ${cmd.help.name}`);
   message.reply(`As of April 2022, Discord will be removing bot access to reading messages, meaning \`${settings.prefix}\` prefixed commands will no longer work. Please begin using the equivalent command \`/${cmd.help.name}\``);
   cmd.run(client, message, args, level);
 };
